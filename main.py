8a2d2ccb3e74e2fd034ccd01f5c75fd0e857e442
Alex007-Biz <bizukin@yandex.ru>
2024-03-28 06:36:24 +0300

    ne code1

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
 
-print("conflict")
-
-for i in range(5):
-    print(f"слово")
-a, b = 9, 3
-if a > b:
-    print("a is greater than b")
-elif b > a:
-    print("b is greater than a")
-else:
-    print("a is equal to b")
+a = 5
 
