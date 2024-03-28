18c49afc11bc23595919d3c5c73830a698eeef72
Alex007-Biz <bizukin@yandex.ru>
2024-03-28 06:12:59 +0300

    conflict

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-print("hello")
-a = 7 # создание переменной
-b = 9
-if a > b:
-    print("a is greater than b")
-elif b > a:
-    print("b is greater than a")
-else:
-    print("a is equal to b")
-    
+print("conflict")
